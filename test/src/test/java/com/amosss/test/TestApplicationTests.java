c41c50b32975aca1c45105d82a6d2a66ae04000a
Amos <1641122430@qq.com>
2023-02-03 16:49:23 +0800

    add springboot

---
+++ test/src/test/java/com/amosss/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amosss.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
